4e65b7b355ec25898bb29d7286d9e88b596f3baf
felix_vondee <felixvondee8@gmail.com>
2021-04-02 03:23:08 +0200

    Add test for joke_spec modify spec_helper

--- spec/jokes_spec.rb
+++ spec/jokes_spec.rb
@@ -7,7 +7,12 @@ describe Joke do
   it 'should create a joke instance' do
     expect(joke).to be_an_instance_of Joke
   end
-  
+
+  it 'should return a joke' do
+    res = joke.display_joke('mike')
+    expect(res).to be_a String
+  end
+
   it 'should return a joke that contains the name given' do
     name = 'jordan'
     res = joke.display_joke(name)
