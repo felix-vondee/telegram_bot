1403241266776af8467d40a6edc1c34cba7d3984
felix_vondee <felixvondee8@gmail.com>
2021-04-02 03:01:58 +0200

    Add new test for Joke class instantiation

--- spec/jokes_spec.rb
+++ spec/jokes_spec.rb
@@ -3,6 +3,10 @@ require_relative '../lib/jokes'
 
 describe Joke do
   let(:joke) { Joke.new }
+
+  it 'should create a joke instance' do
+    expect(joke).to be_an_instance_of Joke
+  end
   
   it 'should return a joke that contains the name given' do
     name = 'jordan'
